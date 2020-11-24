365d8b5c3077ed22a154ef9a8bf99b54ca43bdee
renesantiago <rene.a.santiago@gmail.com>
2020-11-24 22:16:47 +0000

    Initial commit.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./index.html";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let suits = ["heart", "club", "spade", "diamond"];
+  let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let card = document.querySelector(".card");
+  let cardNumber = document.querySelector("h1");
+
+  card.classList.add(suits[getRandomInt(0, suits.length)]);
+  card.innerHTML = cards[getRandomInt(0, cards.length)];
 };
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
